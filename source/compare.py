20caa05e5957a0b985c4995ed175eb89a55bce19
Mathis Börner <mathis.boerner@googlemail.com>
2020-10-05 15:08:36 +0200

    add falsly deleted folder

---
+++ source/compare.py
@@ -0,0 +1,359 @@
+import pathlib
+import functools
+import json
+import copy
+
+import numpy as np
+import tensorflow as tf
+import click
+import geotiler
+from geotiler.cache import redis_downloader
+import gpxpy
+from gpxpy.gpx import GPXTrackPoint
+from gpxpy.geo import simplify_polyline
+import redis
+from sqlalchemy.orm import sessionmaker
+from scipy.stats import trim_mean
+from scipy.spatial.distance import cdist, pdist, squareform
+import plotly.express as px
+import plotly.graph_objects as go
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+
+from .create_figs import (zoom_map,
+                          apply_model,
+                          get_nn_model,
+                          generate_images_for_segment,
+                          bytes2array,
+                          decompress_gpx,
+                          df_from_points)
+from .model import get_engine_and_model
+
+
+tf.autograph.set_verbosity(0)
+# Verbosity is now 0
+
+STYLES = {
+    'pre': {
+        'border': 'thin lightgrey solid',
+        'overflowX': 'scroll'
+    }
+}
+
+
+COLOR_UNMARKED = '#503047'
+COLOR_MARKED = '#C05746'
+
+SIZE_NORAML = 15
+COLOR_NOMATCH = '#7B68EE'
+COLOR_MATCH = '#32CD32'
+
+def row2dict(row):
+    d = {}
+    for column in row.__table__.columns:
+        d[column.name] = getattr(row, column.name)
+    return copy.deepcopy(d)
+
+
+def prepare_df_for_plotting(df):
+    df['Segment'] = df.index.values + 1
+    return df.rename(columns={"latitude": "Latitude", "longitude": "Longitude"})
+
+def generate_segements_sim(config, session, Segments, Routes, segment_ids, route_ids, matrix):
+    map_options = config['map_options']
+    ref_routes = np.unique(route_ids)
+    ref_files = {i: session.query(Routes.path).filter(Routes.id == int(i)).first().path for i in ref_routes}
+
+    if config['apply']['aggregation'].lower().strip() == 'mean':
+        aggregation_func = np.mean
+    elif config['apply']['aggregation'].lower().strip() == 'median':
+        aggregation_func = np.median
+    elif config['apply']['aggregation'].lower().startswith('trim_mean'):
+        _, _, percentage = config['apply']['aggregation'].partition('::')
+        aggregation_func = functools.partial(trim_mean, proportiontocut=float(percentage))
+    else:
+        raise ValueError(f"{config['apply']['aggregation']} is not a valid aggregation function!")
+    mean_dist = np.empty(len(ref_routes), dtype=float)
+    for i, idx_i in enumerate(ref_routes):
+        mask_i = route_ids == idx_i
+        dist_ij = matrix[:, mask_i]
+        min_dist = np.min(dist_ij, axis=1)
+        mean_dist[i] = aggregation_func(min_dist)
+    
+    mean_dist[mean_dist < 0.006] = np.inf
+    idx_min = np.argmin(mean_dist)
+    path_most_sim = ref_files[ref_routes[idx_min]]
+    route_entry = session.query(Routes).filter(Routes.path == path_most_sim).first()
+    route_mask = route_ids == ref_routes[idx_min]
+    matrix_sim = matrix[:, route_mask]
+    segments = []
+    for idx in segment_ids[route_mask]:
+        segment = session.query(Segments).filter(Segments.id == int(idx)).first()
+        mm = geotiler.Map(extent=(segment.p_0_long, segment.p_0_lat, segment.p_1_long, segment.p_1_lat), zoom=map_options['zoom'])
+        segments.append({'point': GPXTrackPoint(latitude=mm.center[1], longitude=mm.center[0]), 'origin': route_entry.path})
+    return segments, matrix_sim, route_entry.gpx_file
+
+
+
+def generate_img_sim_global(config, session, Segments, matrix, segment_ids, embedding_model, render_map):
+    map_options = config['map_options']
+    idx = np.argmin(matrix, axis=1)
+    min_values = np.min(matrix, axis=1)
+    segment_idx = segment_ids[idx]
+    segments = []
+    images = []
+    entries = []
+    batch_size = config.get('apply').get('batch_size', 16)
+    for (idx, value) in zip(segment_idx, min_values):
+        entry = session.query(Segments).filter(Segments.id == int(idx)).first()
+        entry = row2dict(entry)
+        mm = geotiler.Map(extent=(entry['p_0_long'], entry['p_0_lat'], entry['p_1_long'], entry['p_1_lat']), zoom=map_options['zoom'])
+        entry['point'] = GPXTrackPoint(latitude=mm.center[1], longitude=mm.center[0])
+        entry['image_raw'] = render_map(mm).convert('RGB')
+        img_encoded = tf.reshape(tf.convert_to_tensor(bytes2array(entry['image']), dtype=tf.float32), map_options['encoding_shape'])
+        entry['image_encoded'] = img_encoded
+        entry['dist'] = value
+        del entry['image']
+        entries.append(entry)
+        images.append(entry['image_encoded'])
+        if len(images) == batch_size:
+            images_decoded = apply_model(embedding_model, images, batch_size, fill_up=False, func_name='decode')
+            for img_decoded, entry in zip(images_decoded, entries):
+                entry['image_decoded'] = img_decoded.numpy()
+                segments.append(entry)
+            images, entries = [], []
+    if len(entries) > 0:
+        images_decoded = apply_model(embedding_model, images, batch_size, fill_up=True, func_name='decode')
+        for img_decoded, entry in zip(images_decoded, entries):
+            entry['image_decoded'] = img_decoded.numpy()
+            segments.append(entry)
+    return segments
+
+
+def create_plotly_fig_with_line(config, gpx):
+    map_options = config['map_options']
+    map_options['size'] = (map_options['width'], map_options['height'])
+    raw_points = []
+    for track in gpx.tracks:
+        for segment in track.segments:
+            raw_points.extend(segment.points)
+    raw_points = simplify_polyline(raw_points, max_distance=map_options['smoothing_dist'])
+    df_raw = df_from_points(raw_points)
+    center_lat = (df_raw.latitude.min() + df_raw.latitude.max()) / 2.
+    center_long = (df_raw.longitude.min() + df_raw.longitude.max()) / 2.
+    mm = geotiler.Map(center=(center_long, center_lat), zoom=map_options['zoom'], size=map_options['size'])
+    mm = zoom_map(mm, df_raw.longitude, df_raw.latitude)
+    fig = go.FigureWidget(go.Scattermapbox(
+            lat=df_raw.latitude,
+            lon=df_raw.longitude,
+            hoverinfo="none",
+            showlegend=False,
+            mode='lines'))
+    fig.update_layout(
+        mapbox_style="open-street-map",
+        mapbox=dict(
+            bearing=0,
+            center=go.layout.mapbox.Center(
+                lat=mm.center[1],
+                lon=mm.center[0]),
+            pitch=0,
+            zoom=mm.zoom
+        )
+    )
+    fig.update_traces(line=dict(color="Black", width=0.5))
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig
+
+def apply_model_to_file(config, gpx_file, ref_database, weights):
+    gpx_file = pathlib.Path(gpx_file)
+    embedding_model = get_nn_model(config, weights)
+    client = redis.Redis(**config['redis'])
+    downloader = redis_downloader(client)
+    render_map = functools.partial(geotiler.render_map, downloader=downloader)
+    batch_size = config.get('apply').get('batch_size', 16)
+
+    map_options = config['map_options']
+    map_options['size'] = (map_options['width'], map_options['height'])
+    click.echo('Generating segment images:')
+    try:
+        gpx = gpxpy.parse(gpx_file.open())
+    except gpxpy.gpx.GPXXMLSyntaxException:
+        click.echo(f'{gpx_file} is not a valid GPX file!')
+        return
+    segments = []
+    for track_idx, track in enumerate(gpx.tracks):
+        for segment_idx, segment in enumerate(track.segments):
+            images, entries = [], []
+            for img, info in generate_images_for_segment(segment=segment,
+                                                         size=map_options['size'],
+                                                         zoom=map_options['zoom'],
+                                                         max_distance=map_options['smoothing_dist'],
+                                                         render_map=render_map,
+                                                         show_route=map_options['show_route']):
+                entry = {
+                    'origin': gpx_file,
+                    'track_idx': track_idx,
+                    'segment_idx': segment_idx,
+                    'image_raw': img}
+                entry = {**entry, **info}
+                mm = geotiler.Map(extent=(info['p_0_long'], info['p_0_lat'], info['p_1_long'], info['p_1_lat']), zoom=map_options['zoom'])
+                entry['point'] = GPXTrackPoint(latitude=mm.center[1], longitude=mm.center[0])
+                img_embedding = tf.keras.preprocessing.image.img_to_array(img) / 255.
+                images.append(img_embedding)
+                entries.append(entry)
+                if len(images) == batch_size:
+                    images = apply_model(embedding_model, images, batch_size, fill_up=False, func_name='encode')
+                    images_decoded = apply_model(embedding_model, [*images], batch_size, fill_up=False, func_name='decode')
+                    for i, (img_encoded, img_decoded, entry) in enumerate(zip(images, images_decoded, entries)):
+                        if i == 0:
+                            map_options['encoding_shape'] = img_encoded.shape
+                        entry['image_encoded'] = tf.reshape(img_encoded, [np.prod(img_encoded.shape),]).numpy().flatten()
+                        entry['image_decoded'] = img_decoded.numpy()
+                        segments.append(entry)
+                    images, entries = [], []
+            if len(entries) > 0:
+                images = apply_model(embedding_model, images, batch_size, fill_up=True, func_name='encode')
+                images_decoded = apply_model(embedding_model, [*images], batch_size, fill_up=True, func_name='decode')
+                for img_encoded, img_decoded, entry in zip(images, images_decoded, entries):
+                    entry['image_encoded'] = tf.reshape(img_encoded, [np.prod(img_encoded.shape),]).numpy().flatten()
+                    entry['image_decoded'] = img_decoded.numpy()
+                    segments.append(entry)
+    test_images = np.asarray([entry['image_encoded'] for entry in segments])
+    click.echo('Loading Segments from Database')
+    engine, Routes, Segments = get_engine_and_model(ref_database, train=False)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    images = []
+    route_ids = []
+    segment_ids = []
+    for seg in session.query(Segments.image, Segments.origin, Segments.id):
+        images.append(bytes2array(seg.image))
+        route_ids.append(seg.origin)
+        segment_ids.append(seg.id)
+    images = np.asarray(images)
+    route_ids = np.asarray(route_ids)
+    segment_ids = np.asarray(segment_ids)
+    matrix = cdist(test_images, images, metric=config['apply']['metric'])
+
+    segments_sim, matrix_sim, compressed_gpx_file = generate_segements_sim(config, session, Segments, Routes, segment_ids, route_ids, matrix)
+    segments_sim_global = generate_img_sim_global(config, session, Segments, matrix, segment_ids, embedding_model, render_map)
+
+    fig_test = create_plotly_fig_with_line(config, gpx)
+    fig_sim = create_plotly_fig_with_line(config, gpxpy.parse(decompress_gpx(compressed_gpx_file)))
+    session.close()
+    return fig_test, fig_sim, segments, segments_sim, segments_sim_global, matrix_sim
+
+
+def run_comparison(config, gpx_file, ref_database, weights):
+    fig_test, fig_sim, segments_test, segments_sim, segments_sim_global, matrix_sim = apply_model_to_file(config, gpx_file, ref_database, weights)
+
+    log_matrix = np.log2(1. / matrix_sim)
+    mean, std = np.mean(log_matrix), np.std(log_matrix)
+
+    def normalize_marker(values, min_scale=2., max_scale=20.):
+        values = np.log2(1. / values)
+        normalized = (values - mean) / std
+        sig = 1/(1 + np.exp(-normalized))
+        marker_scaling = sig
+        min_scale = 2.
+        max_scale = 20.
+        return min_scale + (max_scale - min_scale) * marker_scaling
+
+    df_test = df_from_points([entry['point'] for entry in segments_test])
+    df_test = prepare_df_for_plotting(df_test)
+    scatter_test = px.scatter_mapbox(df_test, lat="Latitude", lon="Longitude", hover_data=["Segment"]).data[0]
+    n_markers_test = len(segments_test)
+    colors = [COLOR_UNMARKED] * n_markers_test
+    scatter_test.marker.color = colors
+    fig_test.add_trace(scatter_test)
+    fig_test.update_traces(marker_size=SIZE_NORAML)
+
+    df_sim = df_from_points([entry['point'] for entry in segments_sim])
+    df_sim = prepare_df_for_plotting(df_sim)
+    scatter_sim = px.scatter_mapbox(df_sim, lat="Latitude", lon="Longitude", hover_data=["Segment"]).data[0]
+    n_markers_sim = len(segments_sim)
+    colors = [COLOR_NOMATCH] * n_markers_sim
+    scatter_sim.marker.color = colors
+    fig_sim.add_trace(scatter_sim)
+    fig_sim.update_traces(marker_size=SIZE_NORAML)
+
+    fig_seg_test = go.Figure(
+        px.imshow(np.zeros((config['map_options']['width'], config['map_options']['height'], 3)))
+    )
+    fig_seg_sim = go.Figure(
+        px.imshow(np.zeros((config['map_options']['width'], config['map_options']['height'], 3)))
+    )
+    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+    app = dash.Dash('GPX Similarity', external_stylesheets=external_stylesheets,)
+    app.layout = html.Div([
+        dcc.Store(id='clicked-data'),
+        html.H2(f'Loaded File: {gpx_file}'),
+        html.Div([dcc.Graph(figure=fig_test, id='main-map'),]),
+
+        html.Div([html.Div([html.H5('Selected Segment'),
+                            dcc.Graph(id='fig_seg_ref', figure=fig_seg_test)],
+                           className="six columns"),
+                  html.Div([html.H5('Most Similar Segment'),
+                            dcc.Graph(id='fig_seg_sim', figure=fig_seg_sim)],
+                           className="six columns"),],
+                className="row"),
+        dcc.Checklist(id='checkbox', options=[{'label': 'Show embedded', 'value': 'embedded'}], value=[]),
+        html.H2(f'Most Similar Route: {segments_sim[0]["origin"]}'),
+        html.Div([dcc.Graph(figure=fig_sim, id='sim-map'),]),
+        html.Div(className='row', children=[
+            html.Div([
+                dcc.Markdown("""
+                    **Hover Data**
+
+                    Mouse over values in the graph.
+                """),
+                html.Pre(id='click-data', style=STYLES['pre'])
+            ], className='three columns'),])])
+
+    @app.callback([Output('fig_seg_ref', 'figure'),
+                   Output('fig_seg_sim', 'figure'),
+                   Output('main-map', 'figure'),
+                   Output('sim-map', 'figure')],
+                  [Input('main-map', 'clickData'),
+                   Input('checkbox', 'value')],
+                  [State('main-map', 'figure'),
+                   State('sim-map', 'figure')])
+    def display_click_data(click_data, checkbox_value, figure_test, figure_sim):
+        figure_test = go.FigureWidget(figure_test)
+        figure_sim = go.FigureWidget(figure_sim)
+        try:
+            idx = click_data['points'][0]['customdata'][0]-1
+        except (KeyError, IndexError, TypeError):
+            img_test = np.zeros((config['map_options']['width'], config['map_options']['height'], 3))
+            img_sim = np.zeros((config['map_options']['width'], config['map_options']['height'], 3))
+
+            colors = [COLOR_UNMARKED] * n_markers_test
+            figure_test.data[1].marker.color = colors
+
+            size = [SIZE_NORAML] * n_markers_sim
+            figure_sim.data[1].marker.size = size
+        else:
+            if 'embedded' in checkbox_value:
+                key = 'image_decoded'
+            else:
+                key = 'image_raw'
+            entry_test = segments_test[idx]
+            entry_sim_global = segments_sim_global[idx]
+            img_test = entry_test[key]
+            img_sim = entry_sim_global[key]
+
+            colors = [COLOR_UNMARKED] * n_markers_test
+            colors[click_data['points'][0]['pointIndex']] = COLOR_MARKED
+            figure_test.data[1].marker.color = colors
+
+            size = normalize_marker(matrix_sim[idx, :])
+            figure_sim.data[1].marker.size = size
+
+        fig_seg_test = go.Figure(px.imshow(img_test))
+        fig_seg_sim = go.Figure(px.imshow(img_sim))
+        return fig_seg_test, fig_seg_sim, figure_test, figure_sim
+
+    app.title = 'GPX Similarity'
+    app.run_server(debug=True, use_reloader=False) 
---
+++ source/model.py
@@ -0,0 +1,178 @@
+import pathlib
+from functools import partial
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Date, LargeBinary, Float, Boolean, ForeignKey, Binary
+from sqlalchemy.exc import OperationalError
+from sqlalchemy.sql.expression import func
+from sqlalchemy import text
+
+
+def drop_database(database_name, user, password, host, port):
+    import psycopg2
+    from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+    con = psycopg2.connect(user=user, password=password, host=host, port=port)
+    con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor  = con.cursor()
+    sqlCreateDatabase = f"drop database {database_name};"
+    cursor.execute(sqlCreateDatabase)
+
+
+def create_database(database_name, user, password, host, port):
+    import psycopg2
+    from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+    con = psycopg2.connect(user=user, password=password, host=host, port=port)
+    con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor  = con.cursor()
+    sqlCreateDatabase = f"create database {database_name};"
+    cursor.execute(sqlCreateDatabase)
+
+def get_engine_and_model(database_name, user=None, password=None, host=None, port=None, table_name='osm_images', train=True):
+    if train:
+        if user is None or password is None or host is None or port is None:
+            raise ValueError('For training postgres credentials has to be set. See config.toml for infos!')
+        uri = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database_name}'
+    else:
+        database_path = pathlib.Path(database_name).absolute().with_suffix('.db')
+        uri = f'sqlite+pysqlite:///{database_path}'
+        print(f'{uri=}')
+        
+    engine = create_engine(uri)
+
+    base = declarative_base()
+
+    if not train:
+        class Routes(base):
+            __tablename__ = 'routes'
+            id = Column(Integer, primary_key=True)
+            path = Column(String)
+            dataset = Column(String)
+            route_type = Column(String)
+            gpx_file = Column(LargeBinary)
+
+    class OSMImages(base):
+        __tablename__ = table_name
+        id = Column(Integer, primary_key=True)
+        if train:
+            origin = Column(String)
+            dataset = Column(String)
+            route_type = Column(String)
+        else:
+            embedding_hash = Column(String)
+            origin = Column(Integer, ForeignKey('routes.id'))
+        track_idx = Column(Integer)
+        segment_idx = Column(Integer)
+        idx = Column(Integer)
+        zoom = Column(Integer)
+        p_0_lat = Column(Float)
+        p_0_long = Column(Float)
+        p_1_lat = Column(Float)
+        p_1_long = Column(Float)
+        width = Column(Integer)
+        height = Column(Integer)
+        show_route = Column(Boolean)
+        save_type = Column(Integer)
+        image = Column(LargeBinary)
+
+    create_models = partial(base.metadata.create_all, engine, checkfirst=True)
+
+    if train:
+        try:
+            create_models()
+        except OperationalError:
+            create_database(database_name, user, password, host, port)
+            create_models()
+
+        return engine, OSMImages
+    else:
+        create_models()
+        return engine, Routes, OSMImages
+
+
+def set_seed(session, seed, max_val=1177314959, min_val=0):
+    seed_f = seed/max_val
+    if seed_f > max_val or seed_f < min_val:
+        raise ValueError(f'Seed has to be between {min_val} and {max_val}.')
+    sql = text('select setseed({0});'.format(seed_f))  # save the SEED_VAL per user/visit
+    session.execute(sql)
+
+
+
+def postgres_generator(Session, model, offset=0, total_limit=None, entries_per_query=1000, callback=None):
+    session = Session()
+    query = session.query(model.image)
+
+    n_results = 0
+    if not callable(callback):
+        callback = None
+        
+    while True:
+        if total_limit:
+            if offset >= total_limit:
+                break
+            elif offset + entries_per_query > total_limit:
+                entries_per_query = total_limit - offset
+        result = query.limit(entries_per_query).offset(offset).all()
+        n_results = len(result)
+        for r in result:
+            if callback is not None:
+                yield callback(r)
+            else:
+                yield r
+        if n_results < entries_per_query:
+            break
+        offset += entries_per_query     
+
+
+def _postgres_generator_rnd(query, offset, total_limit, entries_per_query, seed=None, callback=None):
+    if seed is not None:
+        set_seed(query.session, seed)
+    n_results = 0
+    if not callable(callback):
+        callback = None
+        
+    while True:
+        if total_limit:
+            if offset >= total_limit:
+                break
+            elif offset + entries_per_query > total_limit:
+                entries_per_query = total_limit - offset
+        result = query.order_by(func.random()).limit(entries_per_query).offset(offset).all()
+        n_results = len(result)
+        for r in result:
+            if callback is not None:
+                yield callback(r)
+            else:
+                yield r
+        if n_results < entries_per_query:
+            break
+        offset += entries_per_query     
+
+
+def generator_from_query_rnd_order(query, train_test_split=0.3, test=False, entries_per_query=1000, seed=None, return_entries=False, callback=None):
+    entries = query.count()
+    test_offset = int(entries * train_test_split + 0.5)
+    if test:
+        total_limit = test_offset
+        offset = 0
+        length = test_offset
+    else:
+        total_limit = None
+        offset = test_offset
+        length = entries - test_offset
+        
+    generator = _postgres_generator_rnd(query=query, 
+                                        offset=offset,
+                                        total_limit=total_limit,
+                                        entries_per_query=entries_per_query,
+                                        seed=seed,
+                                        callback=callback)
+    if return_entries:
+        return generator, length
+    else:
+        return generator
+        
